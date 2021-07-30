26fd08d64765ab24f22df1e8c8a34e98565d2d3d
lovezcr <3529276711@qq.com>
2021-07-30 23:02:05 +0800

    Site updated: 2021-07-30 23:02:01

--- Windows/configs.js
+++ Windows/configs.js
@@ -1,6 +1,6 @@
 ﻿YL.static = {
   /** “关于”信息 */
-  softwareName: '小玲博客', //网站名。请在此处填写您自己的网站名，如王小明的博客。
+  softwareName: '小玲の同学', //网站名。请在此处填写您自己的网站名，如王小明的博客。
   version: "1.0.0", // 网站版本号
   iconBtnStart: 'html5', //主图标
   author: '小玲',//作者
